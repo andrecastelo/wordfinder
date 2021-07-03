349450915c9669fbba8d44e7059a286c763ef3fc
Andre Castelo <andrecastelo@gmail.com>
2021-07-03 00:00:05 -0300

    tailwind boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  display: block;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <div className="tw-m-auto md:tw-max-w-4xl tw-p-4">
+        <p>Hello</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deserunt tempora ea aliquid iusto perspiciatis eaque, saepe, quis mollitia cumque dicta repellendus optio soluta nostrum eos quasi, facilis iure blanditiis!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    </Wrapper>
+  )
 }
 
 export default App;
