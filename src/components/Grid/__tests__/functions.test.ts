95034d673eac21c5b6c744d06a26ca73ed941f57
Andre Castelo <andrecastelo@gmail.com>
2021-07-03 16:26:52 -0300

    implement helper functions to build lines between two points

---
+++ src/components/Grid/__tests__/functions.test.ts
@@ -0,0 +1,83 @@
+import { getLine } from '../functions';
+
+const p = (x: number, y: number) => ({ x, y });
+
+describe('get a valid line between two points', () => {
+  it('works for adjacent points', () => {
+    const center = p(2, 2);
+    const adjacentPoints = [
+      p(1, 1), p(2, 1), p(3, 1),
+      p(1, 2), p(3, 2),
+      p(1, 3), p(2, 3), p(3, 3),
+    ]
+
+    adjacentPoints.forEach(point => {
+      const line = getLine(center, point);
+      const reverseLine = getLine(point, center);
+
+      expect(line.length).toEqual(2);
+      expect(reverseLine.length).toEqual(2);
+      expect(line).toEqual([ center, point ]);
+      expect(reverseLine).toEqual([ point, center ]);
+    });
+  });
+
+  it('will not add the same point', () => {
+    const result = getLine(p(2, 2), p(2, 2));
+
+    expect(result.length).toEqual(1);
+    expect(result).toEqual([ p(2, 2) ]);
+  });
+
+  it('in the same column', () => {
+    const start = p(0, 0),
+          end = p(0, 7),
+          expected = [0, 1, 2, 3, 4, 5, 6, 7].map(y => p(0, y)),
+          expectedReverse = [7, 6, 5, 4, 3, 2, 1, 0].map(y => p(0, y));
+
+    const result = getLine(start, end);
+    const reverse = getLine(end, start);
+
+    expect(result.length).toEqual(8)
+    expect(reverse.length).toEqual(8);
+    expect(result).toEqual(expected);
+    expect(reverse).toEqual(expectedReverse);
+  });
+
+  it('in the same row', () => {
+    const start = p(0, 0),
+          end = p(7, 0),
+          expected = [0, 1, 2, 3, 4, 5, 6, 7].map(x => p(x, 0)),
+          expectedReverse = [7, 6, 5, 4, 3, 2, 1, 0].map(x => p(x, 0));
+
+    const result = getLine(start, end);
+    const reverse = getLine(end, start);
+
+    expect(result.length).toEqual(8)
+    expect(reverse.length).toEqual(8);
+    expect(result).toEqual(expected);
+    expect(reverse).toEqual(expectedReverse);
+  });
+
+  it('in a diagonal, from top left to bottom right and reverse', () => {
+    const start = p(1, 0),
+          end = p(3, 2);
+
+    const result = getLine(start, end);
+    const reverseResult = getLine(end, start);
+
+    expect(result).toEqual([p(1, 0), p(2, 1), p(3, 2)]);
+    expect(reverseResult).toEqual([p(3, 2), p(2, 1), p(1, 0)])
+  });
+
+it('in a diagonal, from top right to bottom left and reverse', () => {
+    const start = p(3, 0),
+          end = p(1, 2);
+
+    const result = getLine(start, end);
+    const reverseResult = getLine(end, start);
+
+    expect(result).toEqual([p(3, 0), p(2, 1), p(1, 2)])
+    expect(reverseResult).toEqual([p(1, 2), p(2, 1), p(3, 0)]);
+  });
+});
--- src/components/Grid/functions.ts
+++ src/components/Grid/functions.ts
@@ -10,6 +10,62 @@ export const indexToCoordinates = (index: number): Coordinates => {
   }
 };
 
-export const getLine = (start: Coordinates, coord: Coordinates): Coordinates[] => {
+const distance = (start: Coordinates, end: Coordinates): number[] => {
+  const diffX = Math.abs(end.x - start.x) + 1,
+        diffY = Math.abs(end.y - start.y) + 1;
 
+  return [diffX, diffY];
+}
+
+const getStraightLine = (start: Coordinates, coord: Coordinates, direction: 'x' | 'y'): Coordinates[] => {
+  const isReverse = start[direction] > coord[direction];
+
+  return [...Array(Math.abs(start[direction] - coord[direction]) + 1)].map((_, index) => {
+    const p: Coordinates = { x: start.x, y: start.y };
+    p[direction] = start[direction] + (index * (isReverse ? -1 : 1));
+
+    return p;
+  })
+}
+
+const getDiagonal = (start: Coordinates, coord: Coordinates, total: number): Coordinates[] => {
+  const diffX = coord.x - start.x,
+        diffY = coord.y - start.y,
+        deltaX = diffX > 0 ? 1 : -1,
+        deltaY = diffY > 0 ? 1 : -1;
+
+  return [...Array(total)].map((_, index) => {
+    const p: Coordinates = {
+      x: start.x + (index * deltaX),
+      y: start.y + (index * deltaY),
+    }
+
+    return p;
+  })
+}
+
+export const getLine = (start: Coordinates, coord: Coordinates): Coordinates[] => {
+  const [lengthX, lengthY] = distance(start, coord);
+
+  if (start.x === coord.x && start.y === coord.y) {
+    return [ start ];
+  }
+
+  if (start.x === coord.x) {
+    return getStraightLine(start, coord, 'y');
+  }
+
+  if (start.y === coord.y) {
+    return getStraightLine(start, coord, 'x');
+  }
+
+  if (lengthX === lengthY && lengthX === 2) {
+    return [ start, coord ];
+  }
+
+  if (lengthX === lengthY) {
+    return getDiagonal(start, coord, lengthX);
+  }
+
+  return [];
 }
