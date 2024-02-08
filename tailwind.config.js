c55ae1c9775616d20281b6deee3390135832b5fe
Andre Castelo <andrecastelo@gmail.com>
2024-02-07 23:30:25 -0300

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  darkMode: false, // or 'media' or 'class'
   prefix: 'tw-',
   fontFamily: {
     sans: ['Roboto', 'sans-serif'],
