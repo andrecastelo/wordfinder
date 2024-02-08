de6c6c3a5a283f111c8f61c76ff30052763b2532
Andre Castelo <andrecastelo@gmail.com>
2024-02-07 22:27:08 -0300

    fix: issue with malformed url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [react()],
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+        open: true,
+        port: 3000,
+    },
     test: {
         globals: true,
         environment: 'jsdom',
