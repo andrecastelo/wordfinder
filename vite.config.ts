e4b033ef0f602e58ed5760142f48d9d3aaf5a824
Andre Castelo <andrecastelo@gmail.com>
2024-02-21 16:19:53 -0300

    build: simplify nginx config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,32 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react(), viteTsconfigPaths()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    css: true,
-    reporters: ['verbose'],
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*'],
-      exclude: [],
-    }
-  },
-})
-
+export default defineConfig(() => {
+  return {
+    base: "/",
+    plugins: [react(), viteTsconfigPaths()],
+    build: {
+      outDir: "build",
+      assetsDir: "assets",
+      emptyOutDir: true,
+    },
+    server: {
+      open: true,
+      port: 3000,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
+      css: true,
+      reporters: ["verbose"],
+      coverage: {
+        reporter: ["text", "json", "html"],
+        include: ["src/**/*"],
+        exclude: [],
+      },
+    },
+  };
+});
